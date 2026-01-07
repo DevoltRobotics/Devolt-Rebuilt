e3a5115e41e2ae12e23b97425d7be81b3914a004
8a <juanochoatrigo@gmail.com>
2026-01-07 12:56:36 -0600

    prueba

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,6 +8,7 @@ public class Constants {
 
     public static final class PoseinField{ //poses para autoacomodación
         public static final Pose2d frontofapriltag = new Pose2d(7.0, 4.0, new Rotation2d(0));
+        public static final Pose2d humanPlayer = new Pose2d(0, 0, new Rotation2d(0));
 
     }
 
