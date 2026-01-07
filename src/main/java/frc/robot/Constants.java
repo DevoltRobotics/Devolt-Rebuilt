3c754175893161e2bf6ba622f05d8dba2ed7f924
8a <juanochoatrigo@gmail.com>
2026-01-07 12:53:27 -0600

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+
+public class Constants {
+
+    public static final class PoseinField{ //poses para autoacomodación
+        public static final Pose2d frontofapriltag = new Pose2d(7.0, 4.0, new Rotation2d(0));
+
+    }
+
+
+}
