230f3ac423de60bcc9fb4076dadfab614769d7f7
pepefrog-krypto <santiihulk@gmail.com>
2026-02-10 11:33:53 -0600

    transfer

--- src/main/java/frc/robot/Subsystems/TransferSubsystem.java
+++ src/main/java/frc/robot/Subsystems/TransferSubsystem.java
@@ -5,38 +5,73 @@
 package frc.robot.Subsystems;
 
 import com.ctre.phoenix6.CANBus;
-import com.ctre.phoenix6.controls.VelocityDutyCycle;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CANId.CAN_s1;
 import frc.robot.Constants.CANId.CAN_s2;
 
 public class TransferSubsystem extends SubsystemBase {
+  private DigitalInput laserLeft = new DigitalInput(0);
+  private DigitalInput laserRight = new DigitalInput(1);
+
 
   private SparkMax beltMotor = new SparkMax(1, CAN_s1.TransferCan, MotorType.kBrushless);
 
   private TalonFX kickerMotor = new TalonFX(CAN_s2.KickerCan, CANBus.systemCore(2));
-  private VelocityDutyCycle Velocity = new VelocityDutyCycle(0);
+  private DutyCycleOut Power = new DutyCycleOut(0);
 
-  private double desiredBeltSpeed = 0;
-  private double desiredKickerSpeed = 0;
+  private final TalonFXConfiguration kickerConfig = new TalonFXConfiguration();
+  private final SparkMaxConfig beltConfig = new SparkMaxConfig();
 
 
 
-  /** Creates a new TransferSubsystem. */
-  public TransferSubsystem() {}
 
-  @Override
-  public void periodic() {
-    beltMotor.set(desiredBeltSpeed);
-    kickerMotor.setControl(Velocity.withVelocity(desiredKickerSpeed));
+  public TransferSubsystem() {
+    beltConfig
+      .inverted(false)
+      .idleMode(IdleMode.kBrake);
+
+      beltMotor.configure(beltConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+      kickerConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    kickerConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+
+    kickerConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    kickerConfig.CurrentLimits.SupplyCurrentLimit = 35;
+
+    kickerMotor.getConfigurator().apply(kickerConfig);
+
   }
 
+  
   public void setSpeeds(double beltSpeed, double kickerSpeed){
-    desiredBeltSpeed = beltSpeed;
-    desiredKickerSpeed = kickerSpeed;
+    beltMotor.set(beltSpeed);
+    kickerMotor.setControl(Power.withOutput(kickerSpeed));
+  }
+
+  public Command TransferShootCMD(TransferSubsystem transferSubsystem){
+    return new InstantCommand(()->{
+        transferSubsystem.setSpeeds(1, 1);
+    });
+  }
+
+  public Command StopTransferCMD(TransferSubsystem transferSubsystem){
+        return new InstantCommand(()->{
+          transferSubsystem.setSpeeds(0.05, 0);
+        });
   }
 }
